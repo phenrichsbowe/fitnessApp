2ccf86ec34ea3b73be42cbd741cd020e4dd6d0c3
phenrichsbowe <phenrichsbowe@my.wctc.edu>
2025-04-11 14:36:04 -0500

    fix(main.js): import vuetify styles and mdi fonts to fix icons and styles not applying correctly

--- src/main.js
+++ src/main.js
@@ -1,15 +1,25 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
+
 import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
+import * as styles from 'vuetify/styles'
+import { aliases, mdi } from 'vuetify/iconsets/mdi'
+import '@mdi/font/css/materialdesignicons.css'
 
 import App from './App.vue'
 
 const vuetify = createVuetify({
   components,
-  directives
+  directives,
+  styles,
+  icons: {
+    defaultSet: 'mdi',
+    aliases,
+    sets: {
+      mdi,
+    },
+  },
 })
 
 createApp(App).use(vuetify).mount('#app')
